6d928f1c7d4be29678919e03043a7a70ed13f290
Mohd Iskandar Mohd Ali <m.iskandar@moh.gov.my>
2022-01-03 16:04:11 +0800

    Data binding

--- app/Http/Livewire/HelloWorld.php
+++ app/Http/Livewire/HelloWorld.php
@@ -6,8 +6,12 @@ use Livewire\Component;
 
 class HelloWorld extends Component
 {
+    public $name = 'Iskandar Ali';
+    public $loud = false;
+    public $greeting = ['Hello'];
+
     public function render()
     {
-        return view('livewire.hello-world', ['name' => 'Iskandar']);
+        return view('livewire.hello-world');
     }
 }
--- resources/views/livewire/hello-world.blade.php
+++ resources/views/livewire/hello-world.blade.php
@@ -1,3 +1,14 @@
 <div>
-    Hello {{ $name }}
+    <input wire:model="name" type="text">
+    <input wire:model="loud" type="checkbox">
+    <select wire:model="greeting" multiple>
+        <option>Hello</option>
+        <option>Assalamualaikum</option>
+        <option>Namaste</option>
+    </select>
+
+    {{ implode(', ', $greeting) }} {{ $name }}
+    @if ($loud)
+        !
+    @endif
 </div>
